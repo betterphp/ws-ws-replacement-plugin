f700741aa0f5b02c6689a04aede7ec89174a0192
Jacek Kuzemczak <jacek@dataplan.co.uk>
2022-04-21 13:50:27 +0100

    Handle raw path for Jenkius nodes correctly

--- src/main/java/org/jenkinsci/plugins/wswsreplacement/WsWsReplacement.java
+++ src/main/java/org/jenkinsci/plugins/wswsreplacement/WsWsReplacement.java
@@ -1,12 +1,14 @@
 package org.jenkinsci.plugins.wswsreplacement;
 
-import java.io.File;
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.model.Node;
 import hudson.model.Slave;
-import jenkins.model.Jenkins;
 import hudson.model.TopLevelItem;
+import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import jenkins.model.Jenkins;
 import jenkins.slaves.WorkspaceLocator;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.NoExternalUse;
@@ -29,9 +31,19 @@ public class WsWsReplacement extends WorkspaceLocator
     private FilePath getWorkspaceRootForNode(Node node)
     {
         if (node instanceof Jenkins) {
-            String path = ((Jenkins) node).getRawWorkspaceDir();
+            String rawWorkspacePath = ((Jenkins) node).getRawWorkspaceDir();
+            String rootPath = ((Jenkins) node).getRootDir().getAbsolutePath();
 
-            return new FilePath(new File(path));
+            Pattern pattern = Pattern.compile("(.+)[/\\\\][$][{]ITEM_FULL_?NAME[}](.*)");
+            Matcher matcher = pattern.matcher(rawWorkspacePath);
+
+            if (!matcher.matches()) {
+                throw new IllegalStateException("Could not find match in ".concat(rawWorkspacePath));
+            }
+
+            String resolvedPath = matcher.group(1).replace("${JENKINS_HOME}", rootPath);
+
+            return new FilePath(new File(resolvedPath));
         } else if (node instanceof Slave) {
             return ((Slave) node).getWorkspaceRoot();
         } else {
